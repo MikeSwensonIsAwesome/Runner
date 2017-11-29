3a8727c6450416e061783d36a30c3f8424c3914a
Michael Swenson <Felixscort@gmail.com>
2017-11-28 20:58:32 -0700

    Got fileReader in HighScore to work! Wahoo.

--- Runner/HighScore.cs
+++ Runner/HighScore.cs
@@ -23,42 +23,50 @@ namespace Runner
         private StringBuilder scores;
         private SpriteFont highScoreFont;
         private bool readFile = true; //needs to be reinstated
-
-
+        static string curFile = @"Content\ScoreRecords.txt";
+        string allFile;
+        int count;
+        string highscores = File.ReadAllText(curFile);
         public HighScore()
         {
-            Initialize();
+            ReadScoresToSB();
         }
 
         private void Initialize()
         {
-          // ReadScoresToSB();
+            ReadScoresToSB();
         }
 
-        private void ReadScoresToSB()
+        private StringBuilder ReadScoresToSB()
         {
+            StringBuilder sb = new StringBuilder();
+
             try
             {
-                using (StreamReader reader = new StreamReader("Content\\ScoreRecords.txt"))
+                using (StreamReader reader = new StreamReader(@"Content\ScoreRecords.txt"))
                 {
                     do
                     {
-                        scores.Append(reader.ReadToEnd());
-
-                    } while (!reader.EndOfStream);
+                        sb.AppendLine(reader.ReadLine());
+                    } while (!reader.EndOfStream || reader.ReadLine() != null);
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine("High Score Reader failure");
-               Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Message);
             }
+            return sb;
         }
 
         public void HighScoreMenu(GameTime gameTime)
         {
-                //Everytime we enter the Score Menu the builder scores should be updated
-                //Initialize();
+            //Everytime we enter the Score Menu the builder scores should be updated
+            if (readFile)
+            {
+                Initialize();
+                readFile = false;
+            }
 
             KeyboardState aCurrentKeyboardState = Keyboard.GetState();
             //Big HEADS UP Might have to cheese it and use a Key Different than AXYB to navigate menus
@@ -79,10 +87,11 @@ namespace Runner
         public void Draw(SpriteBatch spriteBatch)
         {
             highScore.Draw(spriteBatch);
-            spriteBatch.DrawString(highScoreFont, $"High Scores\n\n {DateTime.Now}", new Vector2(200, 59), 
+            spriteBatch.DrawString(highScoreFont, $"High Scores\n\n{ReadScoresToSB().ToString()}", new Vector2(208, 59),
                 Color.Gold, 0, new Vector2(0, 0), .8f, SpriteEffects.None, 0);
-
         }
     }
 }
 
+//sexy sexy test code
+//{(File.Exists(curFile)? "File exists." : "File does not exist.")}
